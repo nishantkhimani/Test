95f98ecc791b762cc7cabe1bafa45128b20528f7
chiragkhimani <khimanichirag@gmail.com>
2015-09-13 18:48:15 +0530

    OMG

--- TestProject/src/com/chirag/Test2.java
+++ TestProject/src/com/chirag/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("added test2");
 		System.out.println("let me add one more");
+		System.out.println("OMG");
 	}
 }
