7407e16d556e63f348ab25066f9bf4f05dc4919a
chiragkhimani <khimanichirag@gmail.com>
2015-09-13 12:47:06 +0530

    My commit

--- TestProject/src/com/chirag/Test.java
+++ TestProject/src/com/chirag/Test.java
@@ -3,13 +3,8 @@ package com.chirag;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("chirag khimani");
-		System.out.println("This is from workspace1");
-		System.out.println("added in master branch");
+		
 	}
-	
-
 	
 	public void sum(){
 		
