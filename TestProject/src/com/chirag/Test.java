dc209ef491afd2009d50bc06db5f138eb2f9c91c
Hassan <chaudriit@gmail.com>
2016-12-21 21:13:55 -0500

    Test message

--- TestProject/src/com/chirag/Test.java
+++ TestProject/src/com/chirag/Test.java
@@ -11,12 +11,6 @@ public class Test {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
 			// this was looks very ugly
-			System.out.println("Let me add one more line from my side");
-			System.out.println("it's seriouly working for me");
-			System.out.println("I haven't done anything");
-			System.out.println("Again let me add one more");
-			System.out.println("Now I have added one more from chirag khimani");
-			System.out.println("added line 1");
 		}
 	}
 
