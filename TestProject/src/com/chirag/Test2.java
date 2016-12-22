9b725da5fedf849b777e872e5d9bec9350632ab3
nishantkhimani <khimaninishant91@gmail.com>
2016-12-22 08:00:44 +0530

    Delete Test2.java

--- TestProject/src/com/chirag/Test2.java
+++
@@ -1,9 +0,0 @@
-package com.chirag;
-
-public class Test2 {
-	public static void main(String[] args) {
-		System.out.println("added test2");
-		System.out.println("let me add one more");
-		System.out.println("new code");
-	}
-}
